b46d4c49ec88096e6e6e16eefb3b4b3e28273ee9
robin-john-menaa <robinprojectgcp@gmail.com>
2022-01-11 11:28:34 +0400

    Project Files

--- webapi/webapi-01/Controllers/WeatherForecastController.cs
+++ webapi/webapi-01/Controllers/WeatherForecastController.cs
@@ -21,6 +21,7 @@ namespace webapi_01.Controllers
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
+
         }
 
         [HttpGet]
@@ -30,7 +31,7 @@ namespace webapi_01.Controllers
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
+                TemperatureC = rng.Next(0, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
