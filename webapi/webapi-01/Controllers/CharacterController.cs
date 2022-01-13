8e1f1710186e5cecf1f2f3de95dad301b6f8afb5
robin-john-menaa <robinprojectgcp@gmail.com>
2022-01-13 19:22:34 +0400

    Api building

--- webapi/webapi-01/Controllers/CharacterController.cs
+++ webapi/webapi-01/Controllers/CharacterController.cs
@@ -1,9 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
+using webapi_01.Models;
+
 namespace webapi_01.Controllers
 {
     [ApiController]
+    [Route("[controller]")]
     public class CharacterController : ControllerBase
     {
-        
+    private static Character knight = new Character();
+    public IActionResult Get()
+    {
+       return Ok(knight);
+    }
+      
     }
 }
