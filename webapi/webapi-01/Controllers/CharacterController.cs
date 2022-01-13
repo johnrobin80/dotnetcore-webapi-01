f51f762429d2eab8e0e003ef4e7f46e52ea0ca39
robin-john-menaa <robinprojectgcp@gmail.com>
2022-01-13 13:17:17 +0400

    Added controllers and models

---
+++ webapi/webapi-01/Controllers/CharacterController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+namespace webapi_01.Controllers
+{
+    [ApiController]
+    public class CharacterController : ControllerBase
+    {
+        
+    }
+}
---
+++ webapi/webapi-01/Models/RpgClass.cs
@@ -0,0 +1,9 @@
+namespace webapi_01.Models
+{
+    public enum RpgClass
+    {
+        Knight,
+        Mage,
+        Cleric
+    }
+}
