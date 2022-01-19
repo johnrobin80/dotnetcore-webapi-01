10d129b028d9277dfd0acce38087a76eafbd68fe
robin-john-menaa <robinprojectgcp@gmail.com>
2022-01-19 19:20:31 +0400

    DataContext class

--- webapi/webapi-01/Data/DataContext.cs
+++ webapi/webapi-01/Data/DataContext.cs
@@ -1,12 +1,15 @@
 using Microsoft.EntityFrameworkCore;
+using webapi_01.Models;
 
 namespace webapi_01.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options)
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
 
         }
+
+        public DbSet<Character> Characters { get; set; }
     }
 }
